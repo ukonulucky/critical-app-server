0071304445e596d6b05af4194c6b32a363e6867f
ukonulucky <ukonulucky@gmail.com>
2025-05-19 03:11:47 +0100

    get user ip address

--- src/helpers/timeFormater.ts
+++ src/helpers/timeFormater.ts
@@ -1,6 +1,6 @@
+
 import moment from "moment-timezone";
 
-
 export const timeFormaterFunc = (): string => {
-  return moment.tz(Date.now(), "Europe/London").format("hh:mm A");
-  };
+  return moment.tz(Date.now(), "Europe/London").format("MMMM Do, h:mm A"); // e.g., "May 19th, 2:07 PM"
+};
