e8a90f1cf87009300d7095f11542218c2df49c87
ukonulucky <ukonulucky@gmail.com>
2025-05-19 02:44:43 +0100

    get user ip address

--- src/helpers/checkUserIp.ts
+++ src/helpers/checkUserIp.ts
@@ -1,42 +1,37 @@
-import { Response, NextFunction } from "express"
-import axios from "axios"
 import { IpAddressInfo } from "../appTypes/types";
+import axios from "axios"
 
+export const getUserIpFunc = async (ip: string): Promise<IpAddressInfo> => {
+  try {
+    console.log("ipInput", ip);
+    const response = await axios.get(`http://ip-api.com/json/${ip}`);
+    const ipdata = response.data;
 
+    console.log("ipdata", ipdata);
 
-
-
-export const getUserIpFunc = async (ip:string): Promise<IpAddressInfo> => { 
-    try {
-        console.log("ipInput", ip)
-      const response = await axios.get(`http://ip-api.com/json/${ip}`);
-        const ipdata = response.data;
-        console.log("ipdata", ipdata)
-        if (ipdata.status === "fail") { 
-            return {
-                time: Date.now().toString(),
-                ipAddress: "",
-                location: {
-                    country: "",
-                    regionName: ""
-                   },
-                status: ipdata.status
-              }
-        }
-           return {
-            time: Date.now().toString(),
-            ipAddress: ipdata.Ip,
-            location: {
-                country: ipdata.location.country,
-                regionName: ipdata.location.regionName
-               },
-            status: ipdata.location.status
-          }
-      
-    
-    } catch (error: any) {
-        throw new Error(error.message)
+    if (ipdata.status === "fail") {
+      return {
+        time: Date.now().toString(),
+        ipAddress: "",
+        location: {
+          country: "",
+          regionName: ""
+        },
+        status: ipdata.status
+      };
     }
 
+    return {
+      time: Date.now().toString(),
+      ipAddress: ipdata.query, // correct field for IP
+      location: {
+        country: ipdata.country,
+        regionName: ipdata.regionName
+      },
+      status: ipdata.status
+    };
 
-}
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
