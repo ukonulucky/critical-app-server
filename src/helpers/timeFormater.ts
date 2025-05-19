81f04a1a0872fcb31efd194d3530635444953592
ukonulucky <ukonulucky@gmail.com>
2025-05-19 03:02:24 +0100

    get user ip address

--- src/helpers/timeFormater.ts
+++ src/helpers/timeFormater.ts
@@ -1,5 +1,6 @@
+import { timeStamp } from "console";
 import moment from "moment";
 
 export const timeFormaterFunc = (): string => {
-    return moment().format("MMMM Do, h:mm A"); // e.g., "May 19th, 2:07 PM"
+    return moment(Date.now()).format("MMMM Do, h:mm A"); // e.g., "May 19th, 2:07 PM"
   };
